2c6919a070b1a8dfd1729d2bfea3e06604ecef30
Ashutosh Singh <ashutosh.singh.de@gmail.com>
2023-07-13 17:50:56 +0200

    Add code

--- YoloV3/loss.py
+++ YoloV3/loss.py
@@ -4,7 +4,13 @@ import torch
 import torchvision
 
 
-def calculate_iou_loss(target_boxes: torch.Tensor, predicted_boxes: torch.Tensor, device='cpu', ciou: bool = False, giou: bool = False):
+def calculate_iou_loss(
+    target_boxes: torch.Tensor,
+    predicted_boxes: torch.Tensor,
+    device="cpu",
+    ciou: bool = False,
+    giou: bool = False,
+):
     if ciou:
         loss = torchvision.ops.ciou_loss(predicted_boxes, target_boxes)
     elif giou:
@@ -18,7 +24,12 @@ def calculate_iou_loss(target_boxes: torch.Tensor, predicted_boxes: torch.Tensor
     return loss
 
 
-def build_targets(predictions: List[torch.Tensor], targets: torch.Tensor, model: torch.nn.Module, hyperparams: Dict[str, Any]):
+def build_targets(
+    predictions: List[torch.Tensor],
+    targets: torch.Tensor,
+    model: torch.nn.Module,
+    hyperparams: Dict[str, Any],
+):
     """
     1. for each yolo detection layer
 
@@ -33,18 +44,25 @@ def build_targets(predictions: List[torch.Tensor], targets: torch.Tensor, model:
 
     # shape of targets - [batch, 6]
     # Example: [[img_id, class, x, y, w, h]], x, y, w, h all are ratios in terms of image size here
-    
-    anchor_ids = torch.arange(na, device=targets.device, dtype=targets.dtype).view(na, 1).repeat(1, num_targets)
 
-    # shape(targets) -> [nt, 6] 
+    anchor_ids = (
+        torch.arange(na, device=targets.device, dtype=targets.dtype)
+        .view(na, 1)
+        .repeat(1, num_targets)
+    )
+
+    t_indices, t_boxes, t_anchors, t_classes = [], [], [], []
+
+    # shape(targets) -> [nt, 6]
     # shape(targets.repeat(na,1,1)) -> [na, nt, 6]
     # shape(anchor_ids) -> [na, nt], shape(anchor_ids[:, :, None]) -> [na, nt, 1]
     # shape(targets) -> [na, nt, 7]
     targets = torch.cat((targets.repeat(na, 1, 1), anchor_ids[:, :, None]), dim=2)
     scale = torch.ones(7, dtype=targets.dtype, device=targets.device)
 
-
     # shape(predictions) -> [batch, #anchors, grid_y, grid_x, #classes + 5]
+
+    # Prepare targets for each yolo layer
     for layer_idx, yolo_layer in enumerate(model.yolo_layers):
         # Get anchors and get scale them down to feature map space
         # shape(anchors) -> [na, 2]
@@ -58,26 +76,73 @@ def build_targets(predictions: List[torch.Tensor], targets: torch.Tensor, model:
         t = targets * scale
 
         # Now we need to find target to anchor association. Each target can be associated to one/multiple/no anchors
-        # Get rid of anchor-target pairs where the divergence in their widht or height is as big as `target_anchor_ratio`
+        # Get rid of anchor-target pairs where the divergence in their width or height is as big as `target_anchor_ratio`.
+        # And lose the anchor dimention - the anchors are identified by the anchor-id appended to targets.
         if num_targets:
-            r = t[:, :, 4: 6] / anchors[:, None, :]
-            j = torch.max(r, 1 / r).max(2)[0] < hyperparams['target_anchor_ratio']  # compare
+            r = t[:, :, 4:6] / anchors[:, None, :]
+            j = (
+                torch.max(r, 1 / r).max(2)[0] < hyperparams["target_anchor_ratio"]
+            )  # compare
             t = t[j]
         else:
             t = targets[0]
 
+        images_ids, classes = t[:, 0:1].long().T
+
+        # Actual coordinates and wh values in feature map space
+        xy = t[:, 2:4]
+        wh = t[:, 4:6]
+
+        # Cell associations to a grid in feature map scape x=1.2, y=3.4 => i=1, j=3 for association with grid cell (1,3)
+        ij = xy.long()
+        i, j = xy.T
+
+        # anchor ids
+        a_ids = t[:, 6].long()
+
+        # clamp grid cell indices to avoid overflow from feature map scale
+        t_indices.append(
+            (
+                images_ids,
+                a_ids,
+                j.clamp_(0, scale[3].long() - 1),
+                i.clamp_(0, scale[2].long() - 1),
+            )
+        )
+
+        # Subject cell associations again to get proper regression targets, we already have grid associations in t_indices
+        t_boxes.append(torch.cat((xy - ij, wh), 1))
+
+        t_anchors.append(anchors[a_ids])
+        t_classes.append(classes)
+
+    return t_indices, t_boxes, t_anchors, t_classes
 
 
-
-
-
-        
-
-        
-
-def calculate_loss(predictions: List[torch.Tensor], targets: torch.Tensor, model: torch.nn.Module):
+def calculate_loss(
+    predictions: List[torch.Tensor],
+    targets: torch.Tensor,
+    model: torch.nn.Module,
+    hyperparams: Dict[str, Any],
+):
+    # shape(predictions) -> [batch, #anchors, grid_y, grid_x, #5+num_classes]
+    # shape(targets) -> [batch, 6], ([img_id, class, x, y, w, h])
     cls_loss = torch.nn.CrossEntropyLoss()
     obj_loss = torch.nn.BCEWithLogitsLoss()
 
+    t_indices, t_boxes, t_anchors, t_classes = build_targets(
+        predictions, targets, model, hyperparams
+    )
+
     for layer_idx, layer_predictions in enumerate(predictions):
-        pass
+        layer_t_indices, layer_t_boxes = t_indices[layer_idx], t_boxes[layer_idx]
+        layer_t_anchors, layer_t_classes = t_anchors[layer_idx], t_boxes[layer_idx]
+
+        image_ids, anch_ids, j, i = layer_t_indices
+        predicted_box = layer_predictions[image_ids, anch_ids, j, i]
+
+        # Sigmoid to predicted x, y offsets.
+        predicted_xy = predicted_box[:, 0:2].sigmoid()
+        predcited_wh = torch.exp(predicted_box[:, 2:4]) * layer_t_anchors
+
+        predicted_box = torch.cat((predicted_xy, predcited_wh), 1)
--- YoloV3/utils/parse_yolo_configs.py
+++ YoloV3/utils/parse_yolo_configs.py
@@ -9,29 +9,29 @@ from YoloV3.layers import EmptyLayer, DetectionLayer
 
 
 activation_map_from_yolo = {
-    'linear': 'linear',
-    'leaky': 'leaky_relu',
+    "linear": "linear",
+    "leaky": "leaky_relu",
 }
 
 
-def parse_yolo_config(path: Union['Path', str]) -> List[Dict[str, Any]]:
+def parse_yolo_config(path: Union["Path", str]) -> List[Dict[str, Any]]:
     path = Path(path) if isinstance(path, str) else path
-    cfg: List[str] = open(path, 'r').read().split('\n')
+    cfg: List[str] = open(path, "r").read().split("\n")
 
     lines: List[str] = [line.strip() for line in cfg if line.strip()]
-    lines = [line for line in lines if line[0] != '#']
+    lines = [line for line in lines if line[0] != "#"]
 
     blocks: List[Dict[str, Any]] = []
     block: Dict[str, Any] = OrderedDict()
 
     for line in lines:
-        if line[0] == '[':
+        if line[0] == "[":
             if block:
                 blocks.append(block)
             block = OrderedDict()
-            block['type'] = line[1: -1].strip()
+            block["type"] = line[1:-1].strip()
         else:
-            key, value = line.split('=')
+            key, value = line.split("=")
             key, value = key.strip(), value.strip()
             try:
                 value = int(value)
@@ -44,49 +44,61 @@ def parse_yolo_config(path: Union['Path', str]) -> List[Dict[str, Any]]:
     return blocks
 
 
-def get_layers_from_blocks(blocks: List[Dict[str, Any]]) -> Tuple[Dict[str, Any], 'nn.ModuleList']:
+def get_layers_from_blocks(
+    blocks: List[Dict[str, Any]]
+) -> Tuple[Dict[str, Any], "nn.ModuleList"]:
     module_list = nn.ModuleList()
     in_channels: int = 3
     out_channels_list: List[int] = []
 
     for index, block in enumerate(blocks[1:]):
-        type_ = block['type']
+        type_ = block["type"]
         module_block = nn.Sequential()
-        if type_ == 'convolutional':
+        if type_ == "convolutional":
             try:
-                batch_norm = int(block.get('batch_normalize'))
+                batch_norm = int(block.get("batch_normalize"))
             except:
                 batch_norm = 0
 
             batch_norm = bool(batch_norm)
 
-            activation = activation_map_from_yolo[block['activation']]
-            out_channels = int(block['filters'])
-            kernel_size = int(block['size'])
-            stride = int(block['stride'])
+            activation = activation_map_from_yolo[block["activation"]]
+            out_channels = int(block["filters"])
+            kernel_size = int(block["size"])
+            stride = int(block["stride"])
 
             try:
-                padding = int(block['pad'])
+                padding = int(block["pad"])
                 padding = (kernel_size - 1) // 2 if padding else 0
             except:
                 padding = 0
 
-            # Conv bias is set to false if batch norm present, since bias is cancelled when mean is subtracted.
-            module_block = Conv2DBlock(in_channels, out_channels, kernel_size,
-                                       stride, padding, batch_norm, activation, conv_bias=not batch_norm)
-        elif type_ == 'shortcut':
-            module_block.add_module(f'shortcut_{index}', EmptyLayer())
+            # Conv bias is set to false if batch norm present
+            module_block = Conv2DBlock(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                batch_norm,
+                activation,
+                conv_bias=not batch_norm,
+            )
+        elif type_ == "shortcut":
+            module_block.add_module(f"shortcut_{index}", EmptyLayer())
 
-        elif type_ == 'upsample':
-            stride = block['stride']
-            module_block.add_module(f'upsample_{index}', nn.Upsample(scale_factor=stride, mode='bilinear'))
+        elif type_ == "upsample":
+            stride = block["stride"]
+            module_block.add_module(
+                f"upsample_{index}", nn.Upsample(scale_factor=stride, mode="bilinear")
+            )
 
-        elif type_ == 'route':
-            _layers = block['layers']
-            module_block.add_module(f'route_{index}', EmptyLayer())
+        elif type_ == "route":
+            _layers = block["layers"]
+            module_block.add_module(f"route_{index}", EmptyLayer())
 
             if isinstance(_layers, str):
-                _layers = _layers.split(',')
+                _layers = _layers.split(",")
                 start = int(_layers[0].strip())
                 end = int(_layers[1].strip())
             else:
@@ -105,17 +117,22 @@ def get_layers_from_blocks(blocks: List[Dict[str, Any]]) -> Tuple[Dict[str, Any]
             #  If end is given out channels for this index will be concatenation of out channels from start layer and
             # end layer.
             if end < 0:
-                out_channels = out_channels_list[start + index] + out_channels_list[end + index]
+                out_channels = (
+                    out_channels_list[start + index] + out_channels_list[end + index]
+                )
             else:
                 out_channels = out_channels_list[start + index]
 
-        elif type_ == 'yolo':
-            masks = block['mask'].split(',')
+        elif type_ == "yolo":
+            masks = block["mask"].split(",")
             masks = [int(m.strip()) for m in masks]
-            anchors = [int(a.strip()) for a in block['anchors'].split(',')]
-            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+            anchors = [int(a.strip()) for a in block["anchors"].split(",")]
+            anchors = [(anchors[i], anchors[i + 1]) for i in range(0, len(anchors), 2)]
             anchors = [anchors[i] for i in masks]
-            module_block.add_module(f'detection_{index}', DetectionLayer(anchors=anchors, classes=block['classes']))
+            module_block.add_module(
+                f"detection_{index}",
+                DetectionLayer(anchors=anchors, classes=block["classes"]),
+            )
 
         in_channels = out_channels
         out_channels_list.append(out_channels)
